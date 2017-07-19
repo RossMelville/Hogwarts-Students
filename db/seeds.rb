b96809855389f070314866c5358b774460c46513
Ross Melville <rossmelville@hotmail.com>
2017-07-19 13:24:38 +0100

    Method added to student class to return all students.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,11 @@ require_relative ('../models/student.rb')
 
 require ('pry')
 
-Student.delete_all
+Student.delete_all()
 
+student1 = Student.new ({'first_name' => 'Harry', 'last_name' => 'Potter'})
+student1.save()
+student2 = Student.new ({'first_name' => 'Ron', 'last_name' => 'Weasley'})
+student2.save()
+
+p Student.all()
